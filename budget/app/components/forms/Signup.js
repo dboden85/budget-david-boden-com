6270ece0df88cf7c1797fdfca826c813e893ebe7
David <davidboden44@gmail.com>
2024-01-26 17:10:08 -0500

    added signup form

---
+++ budget/app/components/forms/Signup.js
@@ -0,0 +1,19 @@
+const Signup = () => {
+    return (
+        <form className="loginForm">
+            <h2>Signup</h2>
+            <label>First Name</label>
+            <input type="text" />
+            <label>Last Name</label>
+            <input type="text" />
+            <label>Email</label>
+            <input type="email" />
+            <label>Username</label>
+            <input type="text" />
+            <label>Password</label>
+            <input type="password" />
+        </form>
+    )
+}
+
+export default Signup;
--- budget/app/page.js
+++ budget/app/page.js
@@ -1,11 +1,17 @@
+"use client"; // This is a client component
+
 import Image from "next/image";
 import './style.scss';
 import Login from './components/forms/Login';
+import React, { useState } from "react";
+import Signup from "./components/forms/Signup";
 
 export default function Home() {
+  const [toSignUp, setToSignUp] = useState(false);
   return (
     <main>
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </main>
   );
 }
