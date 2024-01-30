ccbd02d989b2fe13d9463acd44eeecf779520a55
David <davidboden44@gmail.com>
2024-01-30 12:48:59 -0500

    changed some of the styles

--- budget/app/dashboard/page.js
+++ budget/app/dashboard/page.js
@@ -45,8 +45,8 @@ export default function Dashboard() {
                 </Card>
             </div>
             <div className="bills-info row">
-                <h2>Bills</h2>
-                <div>
+                <Card>
+                    <h2>Bills</h2>
                     <ul>
                         <li><p className="title">Car Payment</p><p className="amount">$300.00</p></li>
                         <li><p className="title">Phone</p><p className="amount">$100.00</p></li>
@@ -55,11 +55,10 @@ export default function Dashboard() {
                         <li><p className="title">Google One</p><p className="amount">$1.99</p></li>
                         <li><p className="title">Audible</p><p className="amount">$15.00</p></li>
                     </ul>
-                </div>
-            </div>
-            <div className="row">
-                <h2>Paycheck Allocations</h2>
-                <div>
+                </Card>
+                
+                <Card>
+                    <h2>Paycheck Allocations</h2>
                     <ul>
                         <li><p className="title">Savings</p><p className="amount">$300.00</p></li>
                         <li><p className="title">Guild Free Spending</p><p className="amount">$200.00</p></li>
@@ -68,11 +67,11 @@ export default function Dashboard() {
                         <li><p className="title">Child Allowance</p><p className="amount">$20.00</p></li>
                         <li><p className="title">Bills</p><p className="amount">$100.00</p></li>
                     </ul>
-                </div>
+                </Card>
             </div>
             <div className="row">
-                <h2>Monthly Allocations</h2>
-                <div>
+                <Card>
+                    <h2>Monthly Allocations</h2>
                     <ul>
                         <li><p className="title">Savings</p><p className="amount">$1200.00</p></li>
                         <li><p className="title">Guild Free Spending</p><p className="amount">$800.00</p></li>
@@ -81,7 +80,7 @@ export default function Dashboard() {
                         <li><p className="title">Child Allowance</p><p className="amount">$80.00</p></li>
                         <li><p className="title">Bills</p><p className="amount">$400.00</p></li>
                     </ul>
-                </div>
+                </Card>
             </div>
         </main>
       </>
