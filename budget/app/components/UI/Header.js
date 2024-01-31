f73a37eecb91c6821527001b9b51d3e2b66e4ba0
David <davidboden44@gmail.com>
2024-01-30 19:34:04 -0500

    added mobile menu and styles

--- budget/app/components/UI/Header.js
+++ budget/app/components/UI/Header.js
@@ -1,9 +1,33 @@
+"use client"; // This is a client component
+import { useState } from "react";
 
 
 const Header = (props)=>{
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const onMenuOpen = ()=>{
+        setMenuOpen(true);
+    }
+
+    const onMenuClose = ()=>{
+        setMenuOpen(false);
+    }
+
     return(
         <header className={`header ${props.cls}`}>
             <h1>{props.title}</h1>
+            <img onClick={onMenuOpen} className="menu-icon" src="../../menu.svg"/>
+            {menuOpen && (
+                <nav className="site-nav">
+                    <h2>Menu</h2>
+                    <img onClick={onMenuClose} className="close-menu-icon" src="../../close-menu.svg"/>
+                    <ul className="nav-menu">
+                        <li className="nav-item">Paycheck</li>
+                        <li className="nav-item">Bills</li>
+                        <li className="nav-item">Savings</li>
+                    </ul>
+                </nav>
+            )}
         </header>
     )
 }
