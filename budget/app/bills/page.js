549e379131cd7e12e23f468a16efdaf6eba684f5
David <davidboden44@gmail.com>
2024-02-06 10:42:42 -0500

    started work on paycheck page

--- budget/app/bills/page.js
+++ budget/app/bills/page.js
@@ -1,114 +1,115 @@
 "use client";
 import Header from "../components/UI/Header";
 import Card from "../components/UI/Card";
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 
 export default function Bills() {
-  const[billTitle, setBillTitle] = useState('');
-  const[billAmount, setBillAmount] = useState();
+  const [billTitle, setBillTitle] = useState('');
+  const [billAmount, setBillAmount] = useState();
   const [billsTotal, setBillsTotal] = useState(0);
+  const [isFormOpen, setIsFormOpen] = useState(false);
   const [bills, setBills] = useState([
     {
-        id: 0,
-        item: 'Car Payment',
-        amount: 305.00
+      id: 0,
+      item: 'Car Payment',
+      amount: 305.00
     },
     {
-        id: 1,
-        item: 'Phone',
-        amount: 111.33
+      id: 1,
+      item: 'Phone',
+      amount: 111.33
     },
     {
-        id: 2,
-        item: 'Gym',
-        amount: 10.00
+      id: 2,
+      item: 'Gym',
+      amount: 10.00
     },
     {
-        id: 4,
-        item: 'Google One',
-        amount: 1.99
+      id: 4,
+      item: 'Google One',
+      amount: 1.99
     },
     {
-        id: 5,
-        item: 'Audible',
-        amount: 15.00
+      id: 5,
+      item: 'Audible',
+      amount: 15.00
     }
-])
-  const [isFormOpen, setIsFormOpen] = useState(false);
+  ])
 
-  useEffect(()=>{
+
+  useEffect(() => {
     let total = 0;
-    bills.map(bill =>{
+    bills.map(bill => {
       total += bill.amount;
     })
     setBillsTotal(total);
-  },[bills])
+  }, [bills])
 
-  const openUpdateMenuHandler = ()=>{
-    setIsFormOpen(prev =>(!prev));
+  const openUpdateMenuHandler = () => {
+    setIsFormOpen(prev => (!prev));
   }
 
-    const onTitleChangeHandler = (e)=>{
-      setBillTitle(e.target.value);
-    }
+  const onTitleChangeHandler = (e) => {
+    setBillTitle(e.target.value);
+  }
 
-    const onAmountChangeHandler = (e)=>{
-      setBillAmount(e.target.value);
-    }
+  const onAmountChangeHandler = (e) => {
+    setBillAmount(e.target.value);
+  }
 
-    const onSubmitHandler = (e)=>{
-      e.preventDefault();
-      setBills(prev => (
-          [...prev,{
-          id: bills.length + 1,
-          item: billTitle.trim(),
-          amount: parseInt(billAmount)
-        }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setBills(prev => (
+      [...prev, {
+        id: bills.length + 1,
+        item: billTitle.trim(),
+        amount: parseInt(billAmount)
+      }
       ]))
-      setIsFormOpen(false);
-      setBillAmount('');
-      setBillTitle('');
-    }
-
-
-
-    return (
-      <>
-        <Header title="Bills"/>
-        <main>
-            <div className="row total-row">
-              <p>Total Bills: <span>${billsTotal.toFixed(2)}</span></p>
-            </div>
-            <div className="row update-bills">
-              <Card>
-                <h2>Bills</h2>
-                <ul>
-                    {bills.map(bill => {
-                        return(<li key={bill.id}><p className="title">{bill.item}</p><p className="amount">{'$' + bill.amount.toFixed(2)}</p></li>)
-                    })}
-                </ul>
-              </Card>
-              <Card>
-                <h2>Graph will go here</h2>
-                
-              </Card>
-            </div>
-        </main>
-
-        <div className={isFormOpen ? 'open update-form-icon' : 'update-form-icon'}>
-          <img onClick={openUpdateMenuHandler} src="../plus.svg" width="35px" height="35px"/>
-        </div>
-
-        <div className={isFormOpen ? 'open update-form' : 'update-form'}>
-          <form className="form" onSubmit={onSubmitHandler}>
-            <h2>Add Bill</h2>
-            <label>Bill Title</label>
-            <input onChange={onTitleChangeHandler} type="text" value={billTitle} />
-            <label>Amount</label>
-            <input onChange={onAmountChangeHandler} type="number"  value={billAmount}/>
-            <button>Add Bill</button>
-          </form>
-        </div>
-      </>
-    );
+    setIsFormOpen(false);
+    setBillAmount('');
+    setBillTitle('');
   }
+
+
+
+  return (
+    <>
+      <Header title="Bills" />
+      <main>
+        <div className="row total-row">
+          <p>Total: <span>${billsTotal.toFixed(2)}</span></p>
+        </div>
+        <div className="row update-bills">
+          <Card>
+            <h2>Bills</h2>
+            <ul>
+              {bills.map(bill => {
+                return (<li key={bill.id}><p className="title">{bill.item}</p><p className="amount">{'$' + bill.amount.toFixed(2)}</p></li>)
+              })}
+            </ul>
+          </Card>
+          <Card>
+            <h2>Graph will go here</h2>
+
+          </Card>
+        </div>
+      </main>
+
+      <div className={isFormOpen ? 'open update-form-icon' : 'update-form-icon'}>
+        <img onClick={openUpdateMenuHandler} src="../plus.svg" width="35px" height="35px" />
+      </div>
+
+      <div className={isFormOpen ? 'open update-form' : 'update-form'}>
+        <form className="form" onSubmit={onSubmitHandler}>
+          <h2>Add Bill</h2>
+          <label>Bill Title</label>
+          <input onChange={onTitleChangeHandler} type="text" value={billTitle} />
+          <label>Amount</label>
+          <input onChange={onAmountChangeHandler} type="number" value={billAmount} />
+          <button>Add Bill</button>
+        </form>
+      </div>
+    </>
+  );
+}
--- budget/app/paycheck/page.js
+++ budget/app/paycheck/page.js
@@ -1,13 +1,110 @@
+"use client";
 import Header from "../components/UI/Header";
 import Card from "../components/UI/Card";
+import { useState, useEffect } from 'react';
 
 export default function Paycheck() {
-    return (
-      <>
-        <Header title="Paycheck"/>
-        <main>
-            
-        </main>
-      </>
-    );
+  const [expenseTitle, setExpenseTitle] = useState('');
+  const [expenseAmount, setExpenseAmount] = useState();
+  const [expensesTotal, setExpensesTotal] = useState(0);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [expenseItems, setExpenseItems] = useState(
+    [
+      {
+        id: 1,
+        item: 'Guilt Free Spending',
+        amount: 100.00
+      },
+      {
+        id: 2,
+        item: 'Gas',
+        amount: 90.00
+      },
+      {
+        id: 3,
+        item: 'Groceries',
+        amount: 50.00
+      },
+      {
+        id: 4,
+        item: 'Kid\'s Allowance',
+        amount: 20.00
+      }
+    ]
+  )
+
+  useEffect(() => {
+    let total = 0;
+    expenseItems.map(item => {
+      total += item.amount;
+    })
+    setExpensesTotal(total);
+  }, [expenseItems])
+
+  const openUpdateMenuHandler = () => {
+    setIsFormOpen(prev => (!prev));
   }
+
+  const onTitleChangeHandler = (e) => {
+    setExpenseTitle(e.target.value);
+  }
+
+  const onAmountChangeHandler = (e) => {
+    setExpenseAmount(e.target.value);
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setExpenseItems(prev => (
+      [...prev, {
+        id: expenseItems.length + 1,
+        item: expenseTitle.trim(),
+        amount: parseInt(expenseAmount)
+      }
+      ]))
+    setIsFormOpen(false);
+    setExpenseAmount('');
+    setExpenseTitle('');
+  }
+
+
+  return (
+    <>
+      <Header title="Bills" />
+      <main>
+        <div className="row total-row">
+          <p>Total: <span>${expensesTotal.toFixed(2)}</span></p>
+        </div>
+        <div className="row update-bills">
+          <Card>
+            <h2>Bills</h2>
+            <ul>
+              {expenseItems.map(item => {
+                return (<li key={item.id}><p className="title">{item.item}</p><p className="amount">{'$' + item.amount.toFixed(2)}</p></li>)
+              })}
+            </ul>
+          </Card>
+          <Card>
+            <h2>Graph will go here</h2>
+
+          </Card>
+        </div>
+      </main>
+
+      <div className={isFormOpen ? 'open update-form-icon' : 'update-form-icon'}>
+        <img onClick={openUpdateMenuHandler} src="../plus.svg" width="35px" height="35px" />
+      </div>
+
+      <div className={isFormOpen ? 'open update-form' : 'update-form'}>
+        <form className="form" onSubmit={onSubmitHandler}>
+          <h2>Add Bill</h2>
+          <label>Bill Title</label>
+          <input onChange={onTitleChangeHandler} type="text" value={expenseTitle} />
+          <label>Amount</label>
+          <input onChange={onAmountChangeHandler} type="number" value={expenseAmount} />
+          <button>Add Bill</button>
+        </form>
+      </div>
+    </>
+  );
+}
