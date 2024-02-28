970dd42cb7bd7d0e34e60871348b34a5fb9b4dcd
David Boden <david.boden@cuanswers.com>
2024-02-28 14:04:44 -0500

    moved the script as well

--- budget-buddy/pages/_app.js
+++ budget-buddy/pages/_app.js
@@ -1,15 +1,5 @@
 import "@/styles/globals.css";
-import '../style.scss';
-import Head from "next/head";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Script>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />;
 }
