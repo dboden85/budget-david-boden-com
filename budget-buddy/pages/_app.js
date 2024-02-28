290d3a5f2fd85a87f469163f1743b88cb24866bd
David Boden <david.boden@cuanswers.com>
2024-02-28 14:03:45 -0500

    fixed the errors that the links in the head were creating.

--- budget-buddy/pages/_app.js
+++ budget-buddy/pages/_app.js
@@ -1,21 +1,14 @@
 import "@/styles/globals.css";
 import '../style.scss';
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/>
-
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet"></link>
-
+      <Script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      </Head>
+      </Script>
       <Component {...pageProps} />
     </>
   )
