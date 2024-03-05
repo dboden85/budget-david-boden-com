8cf64b62ca5f6f1819dfd2ec90083dd76f50f031
David Boden <davidboden44@gmail.com>
2024-03-05 16:01:14 -0500

    adding adduser api

--- budget-buddy/components/login/Signup.js
+++ budget-buddy/components/login/Signup.js
@@ -1,31 +1,13 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 
 const Signup = (props) => {
-    const [fname, setFname] = useState('');
-    const [lname, setLname] = useState('');
-    const [email, setEmail] = useState('');
-    const [uname, setUname] = useState('');
-    const [pass, setPass] = useState('');
+    let fname = useRef('');
+    let lname = useRef('');
+    let email = useRef('');
+    let uname = useRef('');
+    let pass = useRef('');
 
-    const onFnameChange = (e)=>{
-        setFname(e.target.value);
-    }
-
-    const onLnameChange = (e)=>{
-        setLname(e.target.value);
-    }
-
-    const onEmailChange = (e)=>{
-        setEmail(e.target.value);
-    }
-
-    const onUnameChange = (e)=>{
-	setUname(e.target.value);
-    }
-
-    const onPassChange = (e)=>{
-	setPass(e.target.value);
-    }
+    
 
     const onClickHandler = ()=>{
         props.signUp(false);
@@ -34,30 +16,37 @@ const Signup = (props) => {
     const onSubmitHandler = (e)=>{
         e.preventDefault();
 
-	console.log(`${fname}, ${lname}, ${email}, ${uname}, ${pass}`);
+        fname = fname.current.value;
+        lname = lname.current.value;
+        email = email.current.value;
+        uname = uname.current.value;
+        pass = pass.current.value;
 
-        fetch('http://192.168.0.121:5001/create-user', {
-	    method: 'POST',
-	    headers: {
-	        'Content-type': 'application/json',
-	    },
-	    body: JSON.stringify({
-		fname: fname,
-		lname: lname,
-		email: email,
-        	uname: uname,
-		pass: pass
-	    })
-	})
-	.then(res => {
-	    return res.json();
-	})
-	.then(data => {
-	    console.log(data['message']);
-	})
-	.catch(err => {
-	    console.error(err);
-	})
+        console.log(`${fname}\n${lname}\n${email}\n${uname}\n${pass}`)
+
+
+        fetch('/api/adduser', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+        fname: fname,
+        lname: lname,
+        email: email,
+        uname: uname,
+        pass: pass
+        })
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            alert(data.message);
+        })
+        .catch(err => {
+            console.error(err);
+        })
     };
     
     return (
@@ -66,15 +55,15 @@ const Signup = (props) => {
         <form className="loginForm" onSubmit={onSubmitHandler}>
             <h2>Signup</h2>
             <label>First Name</label>
-            <input onChange={onFnameChange} type="text" />
+            <input ref={fname} type="text" />
             <label>Last Name</label>
-            <input onChange={onLnameChange} type="text" />
+            <input ref={lname} type="text" />
             <label>Email</label>
-            <input onChange={onEmailChange} type="email" />
+            <input ref={email} type="email" />
             <label>Username</label>
-            <input onChange={onUnameChange} type="text" />
+            <input ref={uname} type="text" />
             <label>Password</label>
-            <input onChange={onPassChange} type="password" />
+            <input ref={pass} type="password" />
             <button>Submit</button>
         </form>
         <p onClick={onClickHandler}>Back to Login</p>
