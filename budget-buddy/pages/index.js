8373caf509260600a05f1e27e38bf7f956213831
David Boden <davidboden44@gmail.com>
2024-05-10 11:24:43 -0400

    changes made on production side

--- budget-buddy/pages/index.js
+++ budget-buddy/pages/index.js
@@ -5,7 +5,7 @@ import Login from '../components/login/Login';
 import React, { useEffect, useState, useContext } from "react";
 import Signup from "../components/login/Signup";
 import Header from "../components/UI/Header";
-import Auth from '../components/UX/Auth'
+import Auth from '../components/UX/Auth';
 
 function Home() {
   const [toSignUp, setToSignUp] = useState(false);
